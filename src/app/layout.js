dbf3c58089a4632a6d19003dde09c903f18683d4
Riki225 <79812982+Riki225@users.noreply.github.com>
2024-08-12 16:52:09 +1000

    Added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
       </head>
       
       <body className={inter.className}>
+      <Analytics />
       <ToastContainer />
         {children}
         </body>
